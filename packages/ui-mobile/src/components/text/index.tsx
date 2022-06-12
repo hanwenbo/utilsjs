f091c95f73aa1bd11baaa60d8fddb0ffffc8b6b3
hanwenbo <9476400@qq.com>
2022-06-12 13:48:20 +0800

    字体大小

--- packages/ui-mobile/src/components/text/index.tsx
+++ packages/ui-mobile/src/components/text/index.tsx
@@ -1,19 +1,21 @@
-import {Text as RNText} from "react-native-web";
 import React from "react"
+import {Text as RNText, TextProps as RNTextProps} from "react-native-web";
 import {extractStyleCompose} from "../../common/modifiers"
-import {TextProps as RNTextProps} from "react-native-web/exports/Text/types";
 
 export type TextProps = {
   color?: string
+  size?: number
   children?: string | Text,
   [key: string]: any
 } & RNTextProps
 
-const Text = ({style, color, ...props}: TextProps) => {
+const Text = ({style, color, size, ...props}: TextProps) => {
   const _style = extractStyleCompose('Text', props, style) || {}
   if (!!color && typeof color !== 'undefined') {
-    // @ts-ignore
     _style['color'] = color
+  }
+  if (!!size && typeof size !== 'undefined') {
+    _style['fontSize'] = size
   }
   return <RNText {...props} style={_style} />
 }
