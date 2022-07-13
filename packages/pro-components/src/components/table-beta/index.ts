8c1f18593de5913de3244ce7e1bded3b90f7a390
hanwenbo <9476400@qq.com>
2022-07-13 14:29:08 +0800

    redbud

--- packages/pro-components/src/components/table-beta/index.ts
+++ packages/pro-components/src/components/table-beta/index.ts
@@ -3,26 +3,26 @@ import EditModal from "./edit-modal"
 import DelModal from "./del-modal"
 import ImportModal from "./import-modal"
 import {tableSearch} from "./export-modal"
-import * as TableBetaCom from "./table-beta";
+import TableBeta from "./table-beta";
 import DetailModel from "./detail-modal"
 import {getRequestParams} from "../../utils/get-request-params"
 import {filterDefaultRender} from "../../utils/filter-default-render"
 import {getTableListColumns} from "../../utils/get-table-list-columns"
 import {TableList} from "./table-list";
-import {attachPropertiesToComponent} from "../../utils/attach-properties-to-component";
 
-const  TableBeta =  attachPropertiesToComponent(TableBetaCom, {
-  getTableListColumns,
-  getRequestParams,
-  filterDefaultRender,
-  tableSearch,
-  AddModal,
-  EditModal,
-  DelModal,
-  ImportModal,
-  DetailModel,
-  TableList
-})
+
+TableBeta['getTableListColumns'] = getTableListColumns
+TableBeta['getRequestParams'] = getRequestParams
+TableBeta['filterDefaultRender'] = filterDefaultRender
+TableBeta['tableSearch'] = tableSearch
+TableBeta['AddModal'] = AddModal
+TableBeta['EditModal'] = EditModal
+TableBeta['DelModal'] = DelModal
+TableBeta['ImportModal'] = ImportModal
+TableBeta['DetailModel'] = DetailModel
+TableBeta['TableList'] = TableList
+
+
 export default TableBeta
 
 export {
